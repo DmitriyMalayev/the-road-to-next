d7df065d021e596ad7d98b88d9f39dba74a466fd
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2025-02-07 14:55:45 -0500

    eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,30 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            // Adjust the groups to avoid problematic regular expressions
+            ["^\\u0000"], // Side effect imports
+            ["^@?\\w"], // Packages
+            ["^[^.]"], // Absolute imports
+            ["^\\."], // Relative imports
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "error",
+    },
+    languageOptions: {
+      sourceType: "module",
+      ecmaVersion: "latest",
+    },
+  },
 ];
 
 export default eslintConfig;
