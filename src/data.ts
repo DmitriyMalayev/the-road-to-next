11c2946f4e64bcc5552d9fb794e2066fe5dfe6b8
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2025-02-27 17:36:20 -0500

    placeholder data

--- src/data.ts
+++ src/data.ts
@@ -1,7 +1,28 @@
 const initialTickets = [
-  { id: "1", title: "Ticket 1", content: "Description 1", status: "DONE" },
-  { id: "2", title: "Ticket 2", content: "Description 2", status: "DONE" },
-  { id: "3", title: "Ticket 3", content: "Description 3", status: "OPEN" },
+  {
+    id: "1",
+    title: "Ticket 1",
+    content: "Description 1",
+    status: "DONE" as const,
+  },
+  {
+    id: "2",
+    title: "Ticket 2",
+    content: "Description 2",
+    status: "DONE" as const,
+  },
+  {
+    id: "3",
+    title: "Ticket 3",
+    content: "Description 3",
+    status: "OPEN" as const,
+  },
+  {
+    id: "4",
+    title: "Ticket 4",
+    content: "Description 4",
+    status: "IN_PROGRESS" as const,
+  },
 ];
 
 export default initialTickets;
