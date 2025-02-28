95b2045f2ec68d429958a2dbaf48431b2c877d56
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2025-02-27 22:32:31 -0500

    centered

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -19,38 +19,43 @@ export const TICKET_ICONS = {
 
 const Tickets = () => {
   return (
-<div className="flex flex-col mx-20 gap-y-8 p-4">
-  <header className="text-center">
-    <h1 className="text-2xl font-bold">All Tickets</h1>
-    <p className="text-muted-foreground">Here are all the tickets.</p>
-  </header>
+    <div className="flex flex-col mx-20 gap-y-8 p-4">
+      <header className="text-center">
+        <h1 className="text-2xl font-bold">All Tickets</h1>
+        <p className="text-muted-foreground">Here are all the tickets.</p>
+      </header>
 
-  <div className="border p-4 rounded-lg">
-    <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-      {initialTickets.map((ticket) => (
-        <Card key={ticket.id} className="bg-blue-600 rounded-lg overflow-hidden">
-          <CardHeader>
-            <CardTitle className="flex justify-between items-center">
-              <span className="truncate text-orange-300">{ticket.title}</span>
-              <span>{TICKET_ICONS[ticket.status]}</span>
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p className="text-slate-100">{ticket.content}</p>
-          </CardContent>
-          <CardFooter>
-            <Link
-              className="text-orange-300 underline"
-              href={`/tickets/${ticket.id}`}
+      <div className="p-4 rounded-lg">
+        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {initialTickets.map((ticket) => (
+            <Card
+              key={ticket.id}
+              className="bg-blue-600 rounded-lg overflow-hidden"
             >
-              View Details
-            </Link>
-          </CardFooter>
-        </Card>
-      ))}
-    </ul>
-  </div>
-</div>
+              <CardHeader>
+                <CardTitle className="flex justify-between items-center">
+                  <span className="truncate text-orange-300">
+                    {ticket.title}
+                  </span>
+                  <span>{TICKET_ICONS[ticket.status]}</span>
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="text-slate-100">{ticket.content}</p>
+              </CardContent>
+              <CardFooter>
+                <Link
+                  className="text-orange-300 underline"
+                  href={`/tickets/${ticket.id}`}
+                >
+                  View Details
+                </Link>
+              </CardFooter>
+            </Card>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 };
 
